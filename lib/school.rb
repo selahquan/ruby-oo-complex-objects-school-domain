4e62d290169317b170344f002ec9c97ed3bd71e6
Josh <josh.handon@gmail.com>
2020-12-01 11:47:55 -0500

    commit tree ebc8fcf18b084765a1d6b8f9e7e3b48f8302ca5e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+    attr_accessor :roster, :grade, :name
+    def initialize(name)
+        @name= name
+        @roster = {}
+    end
+
+    #def roster
+    #    roster = {}
+    #end
+    
+    def add_student(student, grade)
+        self.roster[grade] ||= []
+        self.roster[grade] << student   
+    end
+
+    def grade(grade)
+        roster.detect do |x, y|
+            if x == grade
+                return y
+            end
+        end
+    end
+
+    def sort
+        result = {}
+        roster.each do |x,y|
+            result[x] =y.sort
+        end
+        result
+    end
+end
